d0b041a30b164554c9994d8a9df45b76e7cad11b
Arghadeep Dey <deyarghadeep23@gmail.com>
2024-07-04 02:34:33 +0530

    added codes for uploading to s3 and changed imageAdd frontend

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,12 @@ const cors = require('cors');
 const crypto = require('crypto');
 const promisify = require('util').promisify;
 const randomBytes = promisify(crypto.randomBytes);
+const PORT = process.env.PORT || 3000
+const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-/*             Database Schemas                  */
-const { createCarTable } = require('./models/car.js');
-const { createUrlTable } = require('./models/url.js');
-const { createImperfectionTable } = require('./models/imperfection.js');
+// AWS S3 credentials
+let { ACCESS_KEYID, SECRET_ACCESS_KEY, REGION, BUCKET_NAME } = process.env
 
 const corsOptions = {
     origin: '*',
@@ -19,32 +20,48 @@ const corsOptions = {
     allowedHeaders: 'Content-Type,Authorization',
     exposedHeaders: 'Content-Range,X-Content- Range'
 };
-
 app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-
-
-const initializeDatabase = async () => {
-    await createImperfectionTable();
-};
-
-initializeDatabase()
-    .then(() => {
-        console.log('Database initialized successfully.');
-        process.exit(0);
-    })
-    .catch((err) => {
-        console.error('Error initializing database:', err);
-        process.exit(1);
+const s3Client = new S3Client(
+    {
+        region: REGION,
+        credentials: {
+            accessKeyId: ACCESS_KEYID,
+            secretAccessKey: SECRET_ACCESS_KEY
+        }
+    }
+);
+async function generateUploadURL() // provides the url for the image (for putting)
+{
+    const rawBytes = await randomBytes(16); // generates 16 random bytes
+    const imageName = rawBytes.toString('hex'); // converting the bytes into hexadecimal
+    const command = new PutObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: imageName,
+        // Expires: 60,
     });
+    const url = await getSignedUrl(s3Client, command);
+    return url;
+    // return null;
+}
+
+app.get('/s3URL', async (req, res) => {
+    const url = await generateUploadURL();
+    res.json({ ['url']: url });
+});
+
+app.post('/upload', (req, res) => {
+    console.log(req.body);
+    res.send('Hello World')
+})
+
 
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 })
---
+++ Backend/testQuery.js
@@ -0,0 +1,23 @@
+// For running any query as a test :
+
+// Import the query first ... 
+/*             Database Queries                  */
+const { createCarTable } = require('./models/car.js');
+const { createUrlTable } = require('./models/url.js');
+const { createImperfectionTable } = require('./models/imperfection.js');
+
+const runQuery = async () => {
+    // create{Tablename}Table is the format 
+    // change the below line
+    // await createImperfectionTable();
+};
+
+runQuery()
+    .then(() => {
+        console.log('Query executed successfully.');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('Error executing query', err);
+        process.exit(1);
+    });
